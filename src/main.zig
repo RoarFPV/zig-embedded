05fcd28a4062f0e5fd0732cfe566dda64f3ba533
RoarFPV <roarfpv@gmail.com>
2022-07-08 13:42:42 -0700

    Get stack end from linker

--- src/main.zig
+++ src/main.zig
@@ -26,7 +26,7 @@ export const start = core.start;
 
 
 export var vector_table linksection("__flash_start") = mcu.VectorTable{
-    .initial_stack_pointer = 0x20040000,
+    // .initial_stack_pointer = 0x20040000,
      .Reset = .{ .C = core.start },
     .SysTick = .{ .C = SysTick_Handler },
     .HardFault = .{ .C = HardFault_Handler },
@@ -48,7 +48,6 @@ pub const pin_map = .{
 };
 
 pub fn init() anyerror!void {
-    
 // 1. Enable HSE and wait for the HSE to become ready
     regs.RCC.CR.HSEON = 1;
     while (regs.RCC.CR.HSERDY != 1) {}
@@ -57,16 +56,18 @@ pub fn init() anyerror!void {
     regs.RCC.APB1ENR.PWREN = 1;
     regs.PWR.CR1.VOS = 1;
 
+// 3. Configure the FLASH PREFETCH and the LATENCY related settings
+    regs.FLASH.ACR.ARTEN = 1;
+    regs.FLASH.ACR.PRFTEN = 1;
+    regs.FLASH.ACR.LATENCY = 7;
+
 // 4. Configure the PRESCALARS HCLK, PCLK1, PCLK2
-    // regs.RCC.CFGR.HPRE = 0; // DIV 1
-    // regs.RCC.CFGR.PPRE1 = 5; // DIV 4
-    // regs.RCC.CFGR.PPRE2 = 4; // DIV 2
+    regs.RCC.CFGR.HPRE = 0; // DIV 1
+    regs.RCC.CFGR.PPRE1 = 5; // DIV 4
+    regs.RCC.CFGR.PPRE2 = 4; // DIV 2
 
     regs.RCC.CR.PLLON = 0;
     while (regs.RCC.CR.PLLRDY != 0) {}
-
-    regs.PWR.CR1.ODEN = 1;
-    while (regs.PWR.CSR1.ODRDY != 1) {}
 
     // regs.RCC.CIR.* = 0;
 // 5. Configure the MAIN PLL
@@ -89,17 +90,15 @@ pub fn init() anyerror!void {
     regs.RCC.CR.PLLON = 1;
     while (regs.RCC.CR.PLLRDY != 1) {}
 
-    
+    regs.PWR.CR1.ODEN = 1;
+    while (regs.PWR.CSR1.ODRDY != 1) {}
+
     // regs.RCC.CR.HSION = 0;
 
 // 7. Select the clock source and waoit for it to be set
-    regs.RCC.CFGR.SW = 2; // System clock use PLL
-    while (regs.RCC.CFGR.SWS != 2) {}
+    // regs.RCC.CFGR.SW = 2; // System clock use PLL
+    // while (regs.RCC.CFGR.SWS != 2) {}
 
-    // 3. Configure the FLASH PREFETCH and the LATENCY related settings
-    regs.FLASH.ACR.ARTEN = 1;
-    regs.FLASH.ACR.PRFTEN = 1;
-    regs.FLASH.ACR.LATENCY = 7;
 
 // enable instruction and data cache
     regs.SCB.CCR.IC = 1;
@@ -108,60 +107,7 @@ pub fn init() anyerror!void {
 
 pub fn main() anyerror!void {
     {
-        // 1. Enable HSE and wait for the HSE to become ready
-        regs.RCC.CR.HSEON = 1;
-        while (regs.RCC.CR.HSERDY != 1) {}
-
-    // 2. Set the POWER ENABLE CLOCK and VOLTAGE REGULATOR
-        regs.RCC.APB1ENR.PWREN = 1;
-        regs.PWR.CR1.VOS = 1;
-
-    // 4. Configure the PRESCALARS HCLK, PCLK1, PCLK2
-        regs.RCC.CFGR.HPRE = 0; // DIV 1
-        regs.RCC.CFGR.PPRE1 = 5; // DIV 4
-        regs.RCC.CFGR.PPRE2 = 4; // DIV 2
-
-        regs.RCC.CR.PLLON = 0;
-        while (regs.RCC.CR.PLLRDY != 0) {}
-
-        // regs.RCC.CIR.* = 0;
-    // 5. Configure the MAIN PLL
-        // #define PLL_M     8
-        // #define PLL_N     432
-        // #define PLL_P     RCC_PLLP_DIV2 /* 2 */
-        // #define PLL_Q     9
-
-        // #define PLL_SAIN  384
-        // #define PLL_SAIQ  7
-        // #define PLL_SAIP  RCC_PLLSAIP_DIV8
-
-        regs.RCC.PLLCFGR.PLLSRC = 1; // HSE
-        regs.RCC.PLLCFGR.PLLM = 8;
-        regs.RCC.PLLCFGR.PLLN = 432;
-        regs.RCC.PLLCFGR.PLLP = 0; // div 2
-        regs.RCC.PLLCFGR.PLLQ = 9;
-
-    // 6. Enable the PLL and wait for it to become ready
-        regs.RCC.CR.PLLON = 1;
-        while (regs.RCC.CR.PLLRDY != 1) {}
-
-        regs.PWR.CR1.ODEN = 1;
-        while (regs.PWR.CSR1.ODRDY != 1) {}
-
-        // regs.RCC.CR.HSION = 0;
-
-    // 7. Select the clock source and waoit for it to be set
-        // regs.RCC.CFGR.SW = 2; // System clock use PLL
-        // while (regs.RCC.CFGR.SWS != 2) {}
-
-        // 3. Configure the FLASH PREFETCH and the LATENCY related settings
-        // regs.FLASH.ACR.ARTEN = 1;
-        // regs.FLASH.ACR.PRFTEN = 1;
-        // regs.FLASH.ACR.LATENCY = 7;
-
-    // enable instruction and data cache
-        // regs.SCB.CCR.IC = 1;
-        // regs.SCB.CCR.DC = 1;
+        try init();
     }
 
     // const led_pin = microzig.Pin("PC13");
